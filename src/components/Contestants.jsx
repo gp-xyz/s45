c81fcfd9c657398800c4bc754ce5c85267ee28be
g <gabe.patgorski@gmail.com>
2023-10-01 19:02:23 -0700

    testthis

--- src/components/Contestants.jsx
+++ src/components/Contestants.jsx
@@ -1,14 +1,25 @@
-import { useEffect, useState } from 'react';
-import config from './config';
+import React, { useEffect, useState } from 'react';
+import { useAppContext } from './AppContext'; // Import useAppContext
+
 function Contestants() {
   const [stats, setStats] = useState([]);
+  const { contestants, tribes } = useAppContext(); // Destructure contestants and tribes from context
 
   useEffect(() => {
-    fetch(`${config.serverName}/stats`)
-      .then(response => response.json())
-      .then(data => setStats(data))
-      .catch(error => console.error(error));
-  }, []);
+    // Iterate over each contestant and find and count the tribes where they are in the survivors array
+    const calculatedStats = contestants.map(contestant => {
+      // filter tribes where the current contestant is a survivor
+      const relatedTribes = tribes.filter(tribe => tribe.survivors.includes(contestant.name));
+      
+      return {
+        ...contestant,
+        count: relatedTribes.length, // Number of tribes that picked the contestant
+        tribes: relatedTribes.map(tribe => tribe.name) // Names of the tribes that picked the contestant
+      };
+    });
+    
+    setStats(calculatedStats);
+  }, [contestants, tribes]); // Recalculate when contestants or tribes change
 
   return (
     <div className='bubblebox'>
@@ -21,7 +32,7 @@ function Contestants() {
               {person.votedOff === 1 && <div className="overlay"></div>}
             </div>
             <div className='grid grid-cols-1'>
-              <div className='font-bold'>{person.name}: {person.count} selection{person.count > 1 && <text>s</text>}</div>
+              <div className='font-bold'>{person.name}: {person.count} selection{person.count > 1 && <span>s</span>}</div>
               <div>
                 {person.tribes && person.tribes.length > 0 ? (
                   person.tribes.map((item) => <div key={item} className='text-xs'>{item}</div>)
@@ -29,7 +40,6 @@ function Contestants() {
                   <div />
                 )}
               </div>
-
             </div>
           </div>
         ))}
